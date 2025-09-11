af2bf9c670b89b7e2307141a25906d7101a68da5
turksoyomer <omerturksoy91@gmail.com>
2025-09-11 11:09:29 +0300

    ChangeLog

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'Evreka360 API'
 copyright = '2024, Evreka'
 author = 'Evreka'
-version = '1.19.0'
+version = '1.19.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
